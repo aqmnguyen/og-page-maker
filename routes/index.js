d724b32096b4b6af95a8fd4f259fb16795ccc09e
Michael Nguyen <mnguyen@movableink.com>
2018-08-10 22:03:25 +0100

    es6 and making params optional

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	
-	let { imageUrl, title, description } = req.query;
-	imageUrl = (imageUrl) ? decodeURIComponent(imageUrl) : '';
+	let { imageUrl, title, description, url } = req.query;
+	imageUrl 		= (imageUrl) ? decodeURIComponent(imageUrl) : '';
+	title 	 		= (title) ? decodeURIComponent(title) : '';
+	description	= (description) ? decodeURIComponent(description) : '';
+	url 				= (url) ? decodeURIComponent(url) : '';
 
   res.render('index', { 	
   	title,
   	description,
-  	imageUrl
+  	imageUrl,
+  	url
   });
 
 });
